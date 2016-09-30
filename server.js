24f4eb30e413b76ad0f0eefa68e6a9f660043230
Ayesha94 <gupta.ayesh@gmail.com>
2016-09-30 23:20:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res) {
-    res,send('My article will be served here');
+    res.send('My article will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
